70f9732ecf13a691b328485294e004a6b82c2697
Vedant K <gamemaker0042@gmail.com>
2022-05-02 16:37:10 +0530

    fix(scripts/setup): fix all lint issues

--- scripts/setup.js
+++ scripts/setup.js
@@ -12,38 +12,44 @@ import { execa } from 'execa'
 
 const json = JSON
 
-// Exit if we are in the CI environment
-if (process.env.CI) {
-	// Delete this script
-	await deleteDirectory('./scripts', {
-		recursive: true,
-		force: true,
-	})
-
-	process.exit(0)
-}
-
-// The files to replace stuff in
+// Some constants
+const { stdout: gitUsername } = await execa('git', [
+	'config',
+	'--global',
+	'--get',
+	'user.name',
+])
+const currentDirectory = path.basename(process.cwd())
 const sourceFiles = [
 	'{source,test}/**/*.ts',
 	'.github/**/*.{md,yaml}',
 	'*.{md,json,yaml}',
 ]
-// Get the data we need
+
+// Exit if we are in the CI environment
+if (process.env.CI) {
+	// Delete this script
+	await deleteDirectory('./scripts/', {
+		recursive: true,
+		force: true,
+	})
+
+	process.exit(0) // eslint-disable-line unicorn/no-process-exit
+}
+
+// The text to replace
 const { authorName, packageName, repository } = await enquirer.prompt([
 	{
 		type: 'input',
 		name: 'authorName',
 		message: 'Enter your full name',
-		inital: (
-			await execa('git', ['config', '--global', '--get', 'user.name'])
-		).stdout,
+		inital: gitUsername,
 	},
 	{
 		type: 'input',
 		name: 'packageName',
 		message: 'Enter the package name',
-		inital: path.basename(process.cwd()),
+		inital: currentDirectory,
 	},
 	{
 		type: 'input',
@@ -52,8 +58,7 @@ const { authorName, packageName, repository } = await enquirer.prompt([
 			'Enter the name of the repository on GitHub (in the format `username/repository-name`)',
 	},
 ])
-// Replace it in all the files
-for (const fromTo of [
+const replacements = [
 	{
 		from: /Vedant K/g,
 		to: authorName,
@@ -66,10 +71,12 @@ for (const fromTo of [
 		from: /typescript-template/g,
 		to: packageName,
 	},
-])
+]
+// Replace it in all the files
+for (const replacement of replacements)
 	await replace({
 		files: sourceFiles,
-		...fromTo,
+		...replacement,
 	})
 
 // Delete some stuff from `package.json`
@@ -78,8 +85,9 @@ const packageJson = json.parse(await readFile('./package.json'))
 for (const setupDep of ['chalk', 'enquirer', 'execa', 'replace-in-file'])
 	delete packageJson.devDependencies[setupDep]
 // Also replace the `prepare` script with the actual version
-delete packageJson.scripts.setup
 packageJson.scripts.prepare = packageJson.scripts['actual-prepare']
+// Delete the now-unneeded scripts
+delete packageJson.scripts.setup
 delete packageJson.scripts['actual-prepare']
 // Now write this data back
 await writeFile('./package.json', json.stringify(packageJson, undefined, '\t'))
@@ -90,12 +98,12 @@ await execa('pnpm', ['install'])
 // Print out we're done!
 console.info(
 	chalk.bold.green(
-		'\nDone setting up! Now you can go write some amazing code :D\n'
-	)
+		'\nDone setting up! Now you can go write some amazing code :D\n',
+	),
 )
 
 // Delete this script
-await deleteDirectory('./scripts', {
+await deleteDirectory('./scripts/', {
 	recursive: true,
 	force: true,
 })
